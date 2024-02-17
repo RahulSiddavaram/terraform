753cb4c00dc010af772fca5b5a2572617e98367c
RahulSiddavaram <srjk7572@gmail.com>
2024-02-17 18:01:28 +0530

    Terraform

--- for-each/ec2.tf
+++ for-each/ec2.tf
@@ -8,16 +8,16 @@ resource "aws_instance" "roboshop" {
   
 }
 #if web give public ip or else give private ip
-# resource "aws_route53_record" "www" {
-#     for_each = aws_instance.roboshop
-#     zone_id = var.zone_id
-#     name    = "${each.key}.${var.domain}" #interpolation
-#     type    = "A"
-#     ttl     = 1
-#     records = [each.key == "web" ? each.value.public_ip : each.value.private_ip ]
-# }
+resource "aws_route53_record" "www" {
+    for_each = aws_instance.roboshop
+    zone_id = var.zone_id
+    name    = "${each.key}.${var.domain}" #interpolation
+    type    = "A"
+    ttl     = 1
+    records = [each.key == "web" ? each.value.public_ip : each.value.private_ip ]
+}
 
 
-# output "aws_instance_info" {
-#     value = aws_instance.roboshop
-# }
+output "aws_instance_info" {
+    value = aws_instance.roboshop
+}
--- for-each/provider.tf
+++ for-each/provider.tf
@@ -5,9 +5,17 @@ terraform {
       version = "5.36.0"
     }
   }
+
+  backend "s3" {
+    bucket   = "roboshp-remote-state"
+    key      = "foreach-demo"
+    region   = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+    
+  }
 }
 
-
+  
 provider "aws" {
   # Configuration options
   # you can give access ke and secret key here, but security problem
