0d5a6226b57aabe1be56937a1ccf2720439480dc
RahulSiddavaram <srjk7572@gmail.com>
2024-02-19 20:02:47 +0530

    Terraform

---
+++ tfvars/variable.tf
@@ -0,0 +1,38 @@
+variable "ami_id" {
+  type = string
+  default = "ami-0f3c7d07486cad139" 
+}
+
+variable "instances" {
+  type = map
+  default = {
+    mongodb = "t3.medium"
+    mysql = "t3.medium"
+    redis = "t2.micro"
+    rabbitmq = "t2.micro"
+    catalogue = "t2.micro"
+    user = "t2.micro"
+    cart = "t2.micro"
+    shipping = "t2.micro"
+    payment = "t2.micro"
+    web = "t2.micro"
+  } 
+}
+
+variable "zone_id" {
+  default = "Z1037679PWH8BTMNZ3SR" 
+}
+
+variable "domain" {
+  default = "allow-all" 
+}
+
+variable "sg_name" {
+  default = "rahuldevops.cloud" 
+}
+
+variable "sg_cidr" {
+  default = ["0.0.0.0/0"]
+}
+
+
